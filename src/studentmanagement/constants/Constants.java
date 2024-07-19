8c03785441d6845ebfc7007d034a7110c8ecf66c
yuseong <dbtjd7282@gmail.com>
2024-07-19 16:20:51 +0900

    feat : ONLY_ENG_KOR 정규표현식 추가

--- src/studentmanagement/constants/Constants.java
+++ src/studentmanagement/constants/Constants.java
@@ -1,4 +1,4 @@
-package studentmanagement.config;
+package studentmanagement.constants;
 
 public class Constants {
     public static final int ST_NUMBER_MIN = 10000000;
@@ -6,4 +6,5 @@ public class Constants {
 
     public static final int SCORE_MIN = 0;
     public static final int SCORE_MAX = 100;
+    public static final String ONLY_ENG_KOR = "^[a-z가-힣]*$";
 }
