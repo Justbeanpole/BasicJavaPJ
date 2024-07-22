23012e50d6bd843aba165a09c527781f725affbb
yuseong <dbtjd7282@gmail.com>
2024-07-22 17:19:27 +0900

    feat : print 추가 메서드

--- src/studentmanagement/view/ViewPrinter.java
+++ src/studentmanagement/view/ViewPrinter.java
@@ -1,21 +1,39 @@
 package studentmanagement.view;
 
 
-import studentmanagement.student.Student;
+import studentmanagement.user.Student;
 
 public class ViewPrinter {
 
-    public void printTitle(){
-        System.out.println("--------------------");
-        System.out.println(" Student Management ");
-        System.out.println("--------------------");
+    public void printAdminTitle(){
+        System.out.println("----------------------------");
+        System.out.println("  Admin Management Service  ");
+        System.out.println("----------------------------");
     }
+
+    public void printStudentTitle(){
+        System.out.println("----------------------------");
+        System.out.println(" Student Management Service ");
+        System.out.println("----------------------------");
+    }
+    public void printModeSelection() {System.out.println("1.Admin | 2.Student | 3.Exit");}
+    public void printLoginSelect() {System.out.println("1.Register | 2.Login | 3.Exit");}
+
     public void printSelection() {
-        System.out.println("1.Get 2.Read 3.Search 4.Update 5.Delete 6.Exit");
+        System.out.println("1.Create 2.Read 3.Search 4.Update 5.Delete 6.Exit");
         System.out.println("Enter the number");
     }
     public void printEnter(){
         System.out.print("Enter : ");
+    }
+    public void printInputId(){
+        System.out.println("Enter your ID");
+    }
+    public void printInputPw(){
+        System.out.println("Enter your Password");
+    }
+    public void printIsduplicatedId(){
+        System.out.println("That ID already exists. Try again.");
     }
 
     public void printInputNumber() {
@@ -59,7 +77,7 @@ public class ViewPrinter {
     public void printStudentInfo(Student student){
         System.out.println(student.getStudentInfo());
     }
-    public void printisDuplicated(){
+    public void printIsDuplicatedStNum(){
         System.out.println("The student number already exists. Try again.");
     }
     public void printExit(){
