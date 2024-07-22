79291f145fcf19a85de1328b6e77898e8e24fde9
yuseong <dbtjd7282@gmail.com>
2024-07-22 17:18:39 +0900

    feat : ID_PW_RULE 추가

--- src/studentmanagement/constants/Constants.java
+++ src/studentmanagement/constants/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final int SCORE_MIN = 0;
     public static final int SCORE_MAX = 100;
     public static final String ONLY_ENG_KOR = "[a-z가-힣\s]*";
+    public static final String ID_PW_RULE = "[A-Za-z0-9]*";
 }
