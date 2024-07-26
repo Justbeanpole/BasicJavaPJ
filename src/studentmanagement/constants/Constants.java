bb392994cfb22f029ec11f8f78396ec50d74ccb0
yuseong <dbtjd7282@gmail.com>
2024-07-26 12:05:43 +0900

    학번 조건 String으로 수정

--- src/studentmanagement/constants/Constants.java
+++ src/studentmanagement/constants/Constants.java
@@ -1,11 +1,9 @@
 package studentmanagement.constants;
 
 public class Constants {
-    public static final int ST_NUMBER_MIN = 10000000;
-    public static final int ST_NUMBER_MAX = 99999999;
-
     public static final int SCORE_MIN = 0;
     public static final int SCORE_MAX = 100;
     public static final String ONLY_ENG_KOR = "[a-z가-힣\s]*";
     public static final String ID_PW_RULE = "[A-Za-z0-9]*";
+    public static final String STUDENT_NUMBER = "[0-9]{8}";
 }
