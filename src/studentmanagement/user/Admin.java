dc7e992193bb024cc7d8c0bff7faabb89010f7e3
yuseong <dbtjd7282@gmail.com>
2024-07-26 12:03:34 +0900

    Admin 생성자를 통해 초기값 설정

--- src/studentmanagement/user/Admin.java
+++ src/studentmanagement/user/Admin.java
@@ -1,32 +1,10 @@
 package studentmanagement.user;
 
-public class Admin {
-    private String name;
-    private String id;
-    private String pw;
+public class Admin extends User {
 
-    public Admin(String name, String id, String pw) {
-        this.name = name;
-        this.id = id;
-        this.pw = pw;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getPw() {
-        return pw;
-    }
-    public void setPw(String pw) {
-        this.pw = pw;
+    public Admin() {
+        this.name = "YuSeong";
+        this.id = "admin";
+        this.password = "admin123";
     }
 }
