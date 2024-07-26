d809f26b5438ecece12dd29e12e86e71cb62cab8
yuseong <dbtjd7282@gmail.com>
2024-07-26 12:07:15 +0900

    실행 함수 메서드 이름 변경 및 반복문 수정 처음 선택하는 메서드 수정 -> 이름 변경, interface사용

--- src/studentmanagement/client/Client.java
+++ src/studentmanagement/client/Client.java
@@ -2,33 +2,44 @@ package studentmanagement.client;
 
 import studentmanagement.service.*;
 import studentmanagement.view.ViewPrinter;
-
 import static studentmanagement.service.InputValidator.*;
-
 
 public class Client {
     ViewPrinter view = new ViewPrinter();
-    ManagementService ms = ManagementService.getInstance();
 
-    public void execute(){
+    public void run() {
         view.printTitle();
-        while(true){
-            selectFunction();
-            System.out.println();
+        boolean cont = true;
+        while(cont){
+            Service service = selectUser();
+            if (service != null) {
+                service.loginMenu();
+            }
+            cont = !(service == null);
+        }
+        System.out.println();
+    }
+
+    private Service selectUser() {
+        view.printUserSelection();
+        int modeNum = getInput(0,2);
+        switch (modeNum) {
+            case 0 -> {
+                view.printExit();
+                return null;
+            }
+            case 1 -> {
+                return new AdminService();
+            }
+            case 2 -> {
+                return new StudentService();
+            }
+            default -> {
+                view.printError();
+                view.printExit();
+                return null;
+            }
         }
     }
-
-    private void selectFunction(){
-        view.printSelection();
-        int id = getInput(1,6);
-        switch(id){
-            case 1 -> ms.input();
-            case 2 -> ms.read();
-            case 3 -> ms.search();
-            case 4 -> ms.update();
-            case 5 -> ms.delete();
-            case 6 -> ms.exitSystem();
-            default -> ms.errorSystem();
-        };
-    }
 }
+
