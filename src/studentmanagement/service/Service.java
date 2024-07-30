970e7481c9dd447e88979fc3ef6d9dd2d6733465
yuseong <dbtjd7282@gmail.com>
2024-07-30 11:03:26 +0900

    feat : TeacherManagement 추가

--- src/studentmanagement/service/Service.java
+++ src/studentmanagement/service/Service.java
@@ -6,9 +6,10 @@ import studentmanagement.view.ViewPrinter;
 import java.util.Scanner;
 
 public interface Service {
-    StudentManagement st = new StudentManagement();
     Scanner sc = new Scanner(System.in);
     ViewPrinter view = new ViewPrinter();
+    StudentManagement st = new StudentManagement();
+    TeacherManagement th = new TeacherManagement();
     Admin admin = new Admin();
 
     void loginMenu();
